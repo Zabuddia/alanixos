ac0bd58fef886bad41587018369b2d978431c9a5
Alan Fife <fife.alan@protonmail.com>
2026-01-14 18:17:36 -0700

    Made it so the computer can boot

--- hosts/alan-big-nixos/default.nix
+++ hosts/alan-big-nixos/default.nix
@@ -13,8 +13,18 @@
   time.timeZone = "America/Denver";
   system.stateVersion = "25.11";
 
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Host basics
+  i18n.defaultLocale = "en_US.UTF-8";
+
   # Nix basics
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Networking
+  networking.networkmanager.enable = true;
 
   # Firewall
   networking.firewall.enable = true;
