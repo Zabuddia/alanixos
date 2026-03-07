e89dbd8e007f59e0004ac1c777d53e493d0177a9
Alan Fife <fife.alan@protonmail.com>
2026-03-07 14:48:48 -0700

    Edited nrs command

--- home/buddia/sh.nix
+++ home/buddia/sh.nix
@@ -1,8 +1,37 @@
 { config, pkgs, ... }:
 
 {
-  programs.bash.enable = true;
-  programs.bash.shellAliases = {
-    nrs = "sudo nixos-rebuild switch --flake path:/home/buddia/.nixos";
+  programs.bash = {
+    enable = true;
+    initExtra = ''
+      nrs() {
+        local elevate
+        local host
+        local repo
+        local system_path
+
+        host="$(hostname -s)"
+        repo="/home/buddia/.nixos"
+
+        if command -v doas >/dev/null 2>&1; then
+          elevate="doas"
+        elif command -v sudo >/dev/null 2>&1; then
+          elevate="sudo"
+        else
+          echo "nrs: need doas or sudo for activation" >&2
+          return 1
+        fi
+
+        system_path="$(
+          cd "$repo" && \
+          nix build ".#nixosConfigurations.''${host}.config.system.build.toplevel" \
+            --print-out-paths \
+            --no-link \
+            -L "$@"
+        )" || return 1
+
+        "$elevate" "$system_path/bin/switch-to-configuration" switch
+      }
+    '';
   };
 }
