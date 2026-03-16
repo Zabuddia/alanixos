bd27968b2db7f0ec5f1f8dec92ccc78a3ed199be
Zabuddia <fife.alan@protonmail.com>
2026-03-16 14:30:20 -0600

    Added alan-laptop-nixos hardware-configuration.nix

--- home/buddia/programs/codium.nix
+++ home/buddia/programs/codium.nix
@@ -5,10 +5,10 @@
     enable = true;
     package = pkgs.vscodium;
     extensions = with pkgs.vscode-extensions; [
-      anthropic.claude-code
-      saoudrizwan.claude-dev
-      ms-vscode-remote.remote-ssh
-      jnoortheen.nix-ide
+#      anthropic.claude-code
+#      saoudrizwan.claude-dev
+#      ms-vscode-remote.remote-ssh
+#      jnoortheen.nix-ide
     ];
   };
 }
