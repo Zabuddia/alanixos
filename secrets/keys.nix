a54a5e40bd11ab6def6a36ed152abdfebedb462e
zabuddia <fife.alan@protonmail.com>
2026-04-03 14:54:52 -0600

    Add alan-framework-laptop age recipient

--- secrets/keys.nix
+++ secrets/keys.nix
@@ -4,6 +4,10 @@
       recipient = "age1w6a0h33yazg69vdupphhxjk3a6rdmgq5qz6c4l2d637j3grzsclqxyyl7k";
       description = "Primary editor key kept on a laptop/workstation.";
     };
+    alan-framework-laptop = {
+      recipient = "age16ca65fwnny3tjk890qvpmq5hu933auwzfjhzrupz50muynh7wc6q8z9n2c";
+      description = "Primary editor key kept on a laptop/workstation.";
+    };
   };
 
   hosts = {
@@ -21,12 +25,6 @@
       recipient = "age14hvqw052wzn8wlxrevzlgydthcenjc7jh4t76gwqljglknj6maash22pl5";
       description = "Root-only runtime key stored at /var/lib/sops-nix/key.txt.";
     };
-
-    # Add alan-framework-laptop here after generating its age public key:
-    # alan-framework-laptop = {
-    #   recipient = "age1...";
-    #   description = "Root-only runtime key stored at /var/lib/sops-nix/key.txt.";
-    # };
   };
 
   creationRules = [
