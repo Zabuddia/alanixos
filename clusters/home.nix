3d8bd89cfb7e9023852ebac8cb605955b9ed4bee
zabuddia <fife.alan@protonmail.com>
2026-04-28 17:50:36 -0600

    syncthing bug fixes

--- clusters/home.nix
+++ clusters/home.nix
@@ -165,12 +165,7 @@ in
       externalDevices.pixel-fold = {
         id = "BT23SPJ-ICTEBQ7-GJTDRQT-LCUQ773-U63QFZR-472O3YA-2KRJ4KY-AMPZ7AF";
         addresses = [ "tcp://pixel-fold:22000" ];
-        folderSets = [
-          "filebrowser-files"
-          "emulation-azahar"
-          "emulation-dolphin"
-          "emulation-melonds"
-        ];
+        folderSets = [ "filebrowser-files" ];
       };
     };
 
