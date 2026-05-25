11801ebaa4dbe47092e1885a960d15354ab33ab5
zabuddia <fife.alan@protonmail.com>
2026-05-25 00:14:48 -0500

    added alsa-utils

--- modules/desktop/audio.nix
+++ modules/desktop/audio.nix
@@ -1,6 +1,8 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 lib.mkIf config.alanix.desktop.enable {
+  environment.systemPackages = [ pkgs.alsa-utils ];
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
