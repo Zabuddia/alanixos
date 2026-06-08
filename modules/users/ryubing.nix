47e758ddaf178bdb4b1b89bb09e23ab77840343b
zabuddia <fife.alan@protonmail.com>
2026-06-07 19:01:33 -0500

    Made joycons work in ryubing

--- modules/users/ryubing.nix
+++ modules/users/ryubing.nix
@@ -3,6 +3,12 @@
 let
   cfg = config.ryubing;
 
+  joycondControllerMappings = ''
+    060000004e696e74656e646f20537700,Nintendo Switch Combined Joy-Cons,a:b1,b:b0,back:b9,dpdown:b15,dpleft:b16,dpright:b17,dpup:b14,guide:b11,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,misc1:b4,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b2,y:b3,platform:Linux,
+    060000007e0500000620000000000000,Nintendo Switch Combined Joy-Cons,a:b1,b:b0,back:b9,dpdown:b15,dpleft:b16,dpright:b17,dpup:b14,guide:b11,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,misc1:b4,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b2,y:b3,platform:Linux,
+    060000007e0500000820000000000000,Nintendo Switch Combined Joy-Cons,a:b1,b:b0,back:b9,dpdown:b15,dpleft:b16,dpright:b17,dpup:b14,guide:b11,leftshoulder:b5,leftstick:b12,lefttrigger:b7,leftx:a0,lefty:a1,misc1:b4,rightshoulder:b6,rightstick:b13,righttrigger:b8,rightx:a2,righty:a3,start:b10,x:b2,y:b3,platform:Linux,
+  '';
+
   managedSettings = lib.filterAttrs (_: value: value != null) {
     game_dirs = cfg.gameDirs;
     start_fullscreen = cfg.startFullscreen;
@@ -45,6 +51,8 @@ in
   config.home.modules = lib.optionals cfg.enable [
     ({ config, lib, ... }: {
       home.packages = [ cfg.package ];
+
+      xdg.configFile."Ryujinx/SDL_GameControllerDB.txt".text = joycondControllerMappings;
 
       home.activation.writeRyubingSettings = lib.mkIf (managedSettings != { }) (lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         configDir="${config.home.homeDirectory}/.config/Ryujinx"
